e8bde7754f98e2fef4a6baf819f3fa3f02990d11
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-15 22:29:01 -0400

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function gameObject() {
     return {
         home: {
@@ -114,3 +115,237 @@ function gameObject() {
         },
     };
 }
+
+const data = gameObject()
+
+function numPointsScored(playerName) {
+
+    for (const key in data) {
+
+        for (const subKey in data[key]) {
+
+            if (subKey === "players") {
+
+                for (const player in data[key][subKey]) {
+
+                    if (playerName === player) {
+
+                        for (const stat in data[key][subKey][player]) {
+
+                            if (stat === "points") {
+
+                                console.log(stat)
+                                return data[key][subKey][player][stat]
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+function shoeSize(playerName) {
+
+    for (const key in data) {
+
+        for (const subKey in data[key]) {
+
+            if (subKey === "players") {
+
+                for (const player in data[key][subKey]) {
+
+                    if (playerName === player) {
+
+                        for (const stat in data[key][subKey][player]) {
+
+                            if (stat === "shoe") {
+
+                                console.log(stat)
+                                return data[key][subKey][player][stat]
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+function teamColors(teamName) {
+
+    for (const key in data) {
+
+        if (data[key]["teamName"] === teamName) {
+
+            return data[key]["colors"]
+
+        }
+
+    }
+
+}
+
+function teamNames() {
+
+    const teamArray = []
+
+    for (const key in data) {
+
+        teamArray.push(data[key]["teamName"])
+
+    }
+
+    return teamArray
+
+}
+
+function playerNumbers(teamName) {
+
+    const playersNumberArray = []
+
+    for (const key in data) {
+
+        if (data[key]["teamName"] === teamName) {
+
+            for (const subKey in data[key]) {
+
+                console.log(subKey)
+                console.log(data[key][subKey])
+
+                if (subKey === "players") {
+
+                    for (const player in data[key][subKey]) {
+
+                        console.log(player)
+                        console.log(data[key][subKey][player]["number"])
+
+                        playersNumberArray.push(data[key][subKey][player]["number"])
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    return playersNumberArray
+
+}
+
+
+function playerStats(playerName) {
+
+    const playersArray = []
+
+    for (const key in data) {
+
+        for (const subKey in data[key]) {
+
+            if (subKey === "players") {
+
+                for (const player in data[key][subKey]) {
+
+                    console.log(player)
+                    console.log(data[key][subKey][player])
+
+                    if (player === playerName) {
+
+                        return data[key][subKey][player]
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+function bigShoeRebounds() {
+
+    const playersShoeSize = []
+
+    for (const key in data) {
+
+        for (const subKey in data[key]) {
+
+            if (subKey === "players") {
+
+                for (const player in data[key][subKey]) {
+
+                    playersShoeSize.push(data[key][subKey][player]["shoe"])
+
+                }
+
+            }
+
+        }
+
+    }
+
+    const largestShoe = playersShoeSize.reduce((prev, current) => {
+
+                    return (prev > current) ? prev : current
+
+                });
+
+    for (const key in data) {
+
+        for (const subKey in data[key]) {
+
+            if (subKey === "players") {
+
+                for(const player in data[key][subKey]) {
+
+                    const playerStats = data[key][subKey][player]
+
+                    if (playerStats["shoe"] >= largestShoe) {
+
+                        return playerStats["rebounds"]
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+// console.log(numPointsScored("Brendan Hayword"))
+
+// console.log(teamColors("Brooklyn Nets"))
+
+// console.log(teamNames())
+
+// console.log(playerNumbers("Brooklyn Nets"))
+
+// console.log(playerStats("Alan Anderson"))
+
+console.log(bigShoeRebounds())
